aa45fb7d60813b8bb8367efd5de1beca1528777e
PauloR063 <paulorcg063@gmail.com>
2026-02-22 13:50:37 -0300

    estrutura front

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,29 @@
-import { Link, NavLink } from 'react-router-dom'
+import './Header.css'
+import logo from "./image/logo.png";
 
 export default function Header() {
   return (
-    <header className="bg-white border-bottom">
-      <div className="container py-3 d-flex justify-content-between align-items-center">
-        <Link to="/" className="text-decoration-none text-dark">
-          <h1 className="h5 m-0">Casa Pame</h1>
-        </Link>
+    <header>
+      
+      <div className='link-header'>
 
-        <nav className="d-flex gap-3">
-          <NavLink
-            to="/"
-            end
-            className={({ isActive }) =>
-              `text-decoration-none ${isActive ? 'fw-semibold' : ''}`
-            }
-          >
-            Home
-          </NavLink>
+        <div className='link-logo'>
+          <img src={logo} alt="Logo Casa Pame" />
+          <h1>Casa Pame </h1>
+        </div>
+          
+        <div className='link-filter'>
+          <h1>Café</h1>
+          <h1>Pratos</h1>
+          <h1>Categorias</h1>
+        </div>
+          
+        <div className='link-registro'>
+          <h1>Entrar</h1>
+          <h1>Registrar</h1>
+        </div> 
+        
 
-          <NavLink
-            to="/sobre"
-            className={({ isActive }) =>
-              `text-decoration-none ${isActive ? 'fw-semibold' : ''}`
-            }
-          >
-            Sobre
-          </NavLink>
-        </nav>
       </div>
     </header>
   )
