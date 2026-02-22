e8114ef468c798cb84f86d48a9a11abd36f3f25c
PauloR063 <paulorcg063@gmail.com>
2026-02-22 15:20:52 -0300

    layout da class hero completa

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,16 @@ export default function Home() {
   return (
     <div className="home">
       <div className="hero">
-        
+        <div className='Container'>
+          <h1>O sabor autêntico do preparo lento.</h1>
+          <p>Pães de fermentação natural, brunchs inesquecíveis e o melhor café da região. Sinta-se em casa na Casa Pame.</p>
+          <div>
+            <button>Ver Cardapio</button>
+            <button id='button-2'>Nossa Historia</button>
+          </div>
+        </div>
       </div>
+      
     </div>
   )
 }
