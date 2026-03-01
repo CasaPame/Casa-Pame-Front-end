7b030b6ebaa74fa32092eda57c6da79fa5fc34dc
PauloR063 <Paulorcg063@gmail.com>
2026-03-01 10:18:24 -0300

    Implementado os cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,10 @@ export default function Home() {
     <div className="home">
       <div className="hero">
         <div className='Container'>
-          <h1>O sabor autêntico do preparo lento.</h1>
-          <p>Pães de fermentação natural, brunchs inesquecíveis e o melhor café da região. Sinta-se em casa na Casa Pame.</p>
+          <h1>O sabor autêntico <br />
+              do preparo lento.</h1>
+          <p>Pães de fermentação natural, brunchs inesquecíveis e o melhor café<br /> 
+            da região. Sinta-se em casa na Casa Pame.</p>
           <div>
             <button>Ver Cardapio</button>
             <button id='button-2'>Nossa Historia</button>
@@ -22,6 +24,28 @@ export default function Home() {
           <p> <span id='span-paes'>Pães</span> produzidos diariamente com fermento natural (levain) e longos períodos de maturação.</p>
         </div>
       </div>
+
+      <div className='favoritos'>
+        <div>
+          <h1>Favoritos da Casa</h1>
+
+          <p> <span id='span-descrição'>Uma</span> 
+          seleção cuidadosa do que nossos clientes mais <br />
+          amam em nosso menu diário. </p>
+
+        </div>
+
+        <div className='cards'>
+          <div className='card1'>
+            <h1>ola</h1>
+          </div>
+
+          <div>ola1</div>
+        </div>
+
+      </div>
+
+      
     </div>
   )
 }
